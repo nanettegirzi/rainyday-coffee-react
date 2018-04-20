77137b51620f17aad780757a2a5f037e10a16e6f
Nanette Girzi <ngirzi@gmail.com>
2018-04-20 16:49:48 -0700

    update title

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template:'template.ejs',
       appMountId: 'react-app-root',
-      title: 'React Help Queue',
+      title: 'Rainy Day Coffee',
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]
